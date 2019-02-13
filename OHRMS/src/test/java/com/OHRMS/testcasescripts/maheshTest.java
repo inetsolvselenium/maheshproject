a57838ae782d49ec91ac11c6fc151dfff325cc03
sudheer <sudheer@sudheer-PC>
2019-02-13 14:58:14 +0530

    new test case was inserted

---
+++ OHRMS/src/test/java/com/OHRMS/testcasescripts/maheshTest.java
@@ -0,0 +1,14 @@
+package com.OHRMS.testcasescripts;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+public class maheshTest {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+}
